6c0bd0da1bf0cacb56eeea136059f7208318ee76
Newbim <113091447+Newbim@users.noreply.github.com>
2022-12-10 19:01:37 +0900

    Update main.py

--- cc1/main.py
+++ cc1/main.py
@@ -1,4 +1,16 @@
+"""
+These are the URLs that will give you remote jobs for the word 'python'
+
+https://kr.indeed.com/jobs?q=python
+https://weworkremotely.com/remote-jobs/search?term=python
+https://remoteok.io/remote-dev+python-jobs
+
+Good luck!
+"""
+
 from flask import Flask, render_template, request, redirect, send_file
+import math
+from flask_paginate import Pagination, get_page_args
 from cc_scrapper import cc_get_contests_keyword
 from cc_scrapper import cc_get_contests
 from exporter import save_to_file
@@ -7,10 +19,10 @@ from exporter import check_file
 
 from cc_scrapper import cc_get_contests_class_erica
 from cc_scrapper import cc_get_contests_class_seoul
+from cc_scrapper import cc_get_contests_keyword
 
 
-
-app = Flask("CC")
+app = Flask(__name__)
 
 db = load_file()
 if (check_file()):
@@ -20,6 +32,14 @@ else:
   save_to_file(db)
 
 dbSearch = {}
+
+app.template_foder = "templates"
+users = list(range(len(db)))
+
+
+def get_users(offset=0, per_page=9):
+  print(users[offset:offset + per_page])
+  return users[offset:offset + per_page]
 
 
 @app.route("/")
@@ -37,41 +57,106 @@ def search():
     else:
       contests = cc_get_contests_keyword(word, db)
       dbSearch[word] = contests
+    users = list(range(len(contests)))  
+    per_page = 9
+    page, per_page, offset = get_page_args(page_parameter="page",
+                                       per_page = per_page,
+                                       per_page_parameter="per_page")
+    total = len(users)
+    pagination_users = get_users(offset=offset, per_page=per_page)
+    pagination = Pagination(page=page,
+                            per_page=per_page,
+                            total=total,
+                            css_framework='bootstrap4')
   else:
     return redirect("/")
   return render_template("search.html",
-                         resultsNumber=len(contests),
+                         page_num = len(contests),
+                         users=pagination_users,
+                         page=page,
+                         per_page=per_page,
                          searchingBy=word,
+                         pagination=pagination,
                          contests=contests)
 
 
 @app.route("/classAll", methods=['GET'])
 def classAll():
-  # pagenum = request.GET.get("page")
+  # # pagenum = request.GET.get("page")
+  # return render_template("classAll.html",
+  #                        resultsNumber=len(db),
+  #                        searchingBy="ALL",
+  #                        contests=db,
+  #                       )
+  per_page = 9
+  page, per_page, offset = get_page_args(page_parameter="page",
+                                         per_page = per_page,
+                                         per_page_parameter="per_page")
+  total = len(users)
+  pagination_users = get_users(offset=offset, per_page=9)
+  pagination = Pagination(page=page,
+                          per_page=per_page,
+                          total=total,
+                          css_framework='bootstrap4')
+
   return render_template("classAll.html",
-                         resultsNumber=len(db),
-                         searchingBy="ALL",
-                         contests=db,
-                        )
+                         page_num = len(db),
+                         users=pagination_users,
+                         page=page,
+                         per_page=per_page,
+                         pagination=pagination,
+                         contests=db)
 
 
 @app.route("/classErica", methods=['GET'])
 def classErica():
   lst = cc_get_contests_class_erica(db)
+  users = list(range(len(lst)))
+  per_page = 9
+  page, per_page, offset = get_page_args(page_parameter="page",
+                                         per_page = per_page,
+                                         per_page_parameter="per_page")
+  total = len(users)
+  pagination_users = get_users(offset=offset, per_page=per_page)
+  pagination = Pagination(page=page,
+                          per_page=per_page,
+                          total=total,
+                          css_framework='bootstrap4')
+
   return render_template("classErica.html",
-                         resultsNumber=len(lst),
-                         searchingBy="ERICA",
+                         page_num = len(lst),
+                         users=pagination_users,
+                         page=page,
+                         per_page=per_page,
+                         pagination=pagination,
                          contests=lst)
 
 
 @app.route("/classSeoul", methods=['GET'])
 def classSeoul():
   lst = cc_get_contests_class_seoul(db)
+  users = list(range(len(lst)))
+  per_page = 9
+  page, per_page, offset = get_page_args(page_parameter="page",
+                                         per_page = per_page,
+                                         per_page_parameter="per_page")
+  total = len(users)
+  pagination_users = get_users(offset=offset, per_page=9)
+  pagination = Pagination(page=page,
+                          per_page=per_page,
+                          total=total,
+                          css_framework='bootstrap4')
+
   return render_template("classSeoul.html",
-                         resultsNumber=len(lst),
-                         searchingBy="SEOUL",
+                         page_num = len(lst),
+                         users=pagination_users,
+                         page=page,
+                         per_page=per_page,
+                         pagination=pagination,
                          contests=lst)
 
 
-app.run(host="0.0.0.0")
-
+if __name__ == "__main__":
+  # app.run(debug=True)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
